a68d9a14a884d125206020f65c30bfdc9d3da0d0
stap780 <panaet80@mail.ru>
2022-11-05 19:29:27 +0300

    Update countable.rb

--- lib/insales_api/resource/countable.rb
+++ lib/insales_api/resource/countable.rb
@@ -2,6 +2,9 @@ module InsalesApi
   module Resource
     module Countable
       def count(options = {})
+        puts get(:count, options)
+        puts get(:count, options)['count']
+        puts get(:count, options)['count'][0]
         get(:count, options)['count'][0].to_i
         #get(:count, options).to_i
       end
