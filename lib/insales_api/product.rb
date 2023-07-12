1785228245ab033da17540108f86871ccecc3058
stap780 <panaet80@mail.ru>
2023-07-12 20:53:02 +0300

    Update product.rb
    now we can update group of variants

--- lib/insales_api/product.rb
+++ lib/insales_api/product.rb
@@ -1,5 +1,27 @@
 module InsalesApi
   class Product < Base
     extend Resource::WithUpdatedSince
+
+    class << self
+      
+      # Updates all given variants. +variants+ should be array:
+      #
+      #   [
+      #     {
+      #       id: 1,
+      #       price: 340,
+      #       quantity: 4,
+      #     },
+      #     {
+      #       id: 2,
+      #       price: 350,
+      #       quantity: 5,
+      #     },
+      #   ]
+      def variants_group_update(variants)
+        put(:variants_group_update, {}, format.encode(variants, root: :variants))
+      end
+    end
+    
   end
 end
